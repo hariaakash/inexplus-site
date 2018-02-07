88615000b2f8d42c6ff5c8bb4de539dabfd43305
Hari Narayanan <smgdark@gmail.com>
2018-02-08 00:51:48 +0530

    modal images added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 (function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
+  "use strict";
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -14,5 +12,4 @@
       }
     }
   });
-
-})(jQuery); // End of use strict
+})(jQuery);
