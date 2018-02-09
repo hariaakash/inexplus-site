572bb200a1ef010fa417dc7bf674b15b0b53bfb5
Hari Narayanan <smgdark@gmail.com>
2018-02-10 01:22:15 +0530

    new contact.php file

--- contact.php
+++ contact.php
@@ -1,34 +1,84 @@
 <?php
 
-if (isset($_POST["submit"])) {
+if(isset($_POST['email'])) {
 
-    // Checking For Blank Fields..
-    if ($_POST["name"]==""||$_POST["email"]==""||$_POST["number"]==""||$_POST["message"]=="") {
-        echo "Fill All Fields..";
-    } else {
-        // Check if the "Sender's Email" input field is filled out
-        $email=$_POST['email'];
-        // Sanitize E-mail Address
-        $email =filter_var($email, FILTER_SANITIZE_EMAIL);
-        // Validate E-mail Address
-        $email= filter_var($email, FILTER_VALIDATE_EMAIL);
-        if (!$email) {
-            echo "Invalid Sender's Email";
-        } else {
-            $name = $_POST['name'];
-            $number = $_POST['number'];
-            $message = $_POST['message'];
-            $headers = 'From:'. $email . "\r\n"; // Sender's Email
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "info@inexplus.in";
 
-            $message = $message . ' ' . $number
-
-            // Message lines should not exceed 70 characters (PHP rule), so wrap it
-            $message = wordwrap($message, 70);
-
-            // Send Mail By PHP Mail Function
-            mail("info@inexplus.in", "Mail from " . $name , $message, $headers);
-            echo "Thank You, we will be in touch with you very soon.";
-        }
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
     }
+
+
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['number']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+
+
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $number = $_POST['number']; // required
+    $message = $_POST['message']; // required
+    $email_subject = "Contact form submitted by" . $_POST['name'];
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+
+  if(strlen($message) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+
+
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($number)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
+
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+<!-- include your own success html here -->
+
+Thank you for contacting us. We will be in touch with you very soon.
+
+<?php
+
 }
 ?>
